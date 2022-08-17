08c3d322169a8ee55c112db5414756d03f07ae8a
Damián <dcopik25@gmail.com>
2022-08-17 17:23:16 +0200

    ✨ feat: Add page loader

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,7 @@
-function closeNav() {
+const closeNav = () => {
   document.getElementById("menu-control").checked = false;
-}
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  document.getElementsByClassName("loading")[0].remove();
+});
