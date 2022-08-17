84a96be109b81843243ec90d46a7f60494e1190a
Damián <dcopik25@gmail.com>
2022-08-17 21:35:48 +0200

    ✨ feat: Add lazy image loading

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,16 @@ const closeNav = () => {
   document.getElementById("menu-control").checked = false;
 };
 
-document.addEventListener("DOMContentLoaded", () => {
+window.addEventListener("load", () => {
   document.getElementsByClassName("loading")[0].remove();
+  loadImages();
 });
+
+const loadImages = () => {
+  const images = document
+    .getElementById("galeria")
+    .getElementsByClassName("photos")[0].children;
+  for (const img of images) {
+    img.src = img.attributes._src.value;
+  }
+};
